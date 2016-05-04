6d1e26a8ccb535046ccc029ef569a89458a865f4
Flavio Copes <copesc@gmail.com>
2016-05-04 19:11:22 +0200

    Correctly load the user's data as soon as the user is logged in, also for the first time

--- classes/Controller.php
+++ classes/Controller.php
@@ -333,7 +333,6 @@ class Controller extends \Grav\Plugin\Login\Controller
             $user->authenticated = true;
             $user->save();
 
-            $user = User::load($username);
         } else {
             $authenticated = $user->authenticate($password);
         }
